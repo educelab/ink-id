edf8443a553f363574880a0267c7dc06458ba663
Jack Bandy <jgba225@g.uky.edu>
2017-01-25 13:29:57 -0500

    added next_batch_3d method to trainvol

--- trainvol.py
+++ trainvol.py
@@ -13,7 +13,7 @@ from skimage.transform import resize
 
 class TrainVol(object):
 
-    def __init__(self, volume_fn, truth_fn, CUT_IN, CUT_BACK, NR, THRESH, RSZ):
+    def __init__(self, volume_fn, truth_fn, CUT_IN, CUT_BACK, NR, THRESH, RSZ, DEPTH=0):
         print("initializing TrainVol...")
         self.current_spot = 0
         self.fragment_buffer = 4
@@ -21,6 +21,7 @@ class TrainVol(object):
         self.C_IN = CUT_IN
         self.C_BACK = CUT_BACK
         self.RSZ = RSZ
+        self.DEPTH = DEPTH
 
         print("loading raw volume...")
         self.raw_volume = np.load(volume_fn).astype(np.float64)
@@ -38,12 +39,12 @@ class TrainVol(object):
         self.ground_truth = np.zeros(self.raw_ground_truth.shape, dtype=np.float32)
         for i in range(self.NR, self.ground_truth.shape[0] - self.NR):
             for j in range(self.NR, self.ground_truth.shape[1] - self.NR):
-                # make sure all labels in the neighborhood are ink
+                # make sure all labels in the neighborhood are unanimously ink or not
                 t_mean = np.mean(self.raw_ground_truth[i-NR:i+NR, j-NR:j+NR])
                 if t_mean > .9:
                     self.ground_truth[i,j] = .99
                 elif t_mean < .1:
-                    self.ground_truth[i,j] = .01
+                    self.ground_truth[i,j] = .00
                 '''
                 else:
                     self.ground_truth[i,j] = t_mean / 2
@@ -59,17 +60,19 @@ class TrainVol(object):
 
 
     def next_batch(self, batch_size):
+        """Returns 'batch_size' 2D training samples"""
         width = 2*self.NR
         height = self.C_IN + self.C_BACK
         to_return_x = np.zeros((batch_size, width, height), dtype=np.float32)
         to_return_y = np.zeros((batch_size, 2), dtype=np.float32)
 
+        # if everything has been trained on, loop back
         if(self.current_spot + batch_size > self.total_vects):
             self.current_spot = 0
             np.random.shuffle(self.train_inds)
 
         start = self.current_spot
-        end = start + batch_size
+        end = start + batch_size 
 
         batch_inds = self.train_inds[start:end]
         c = 0
@@ -81,14 +84,63 @@ class TrainVol(object):
             surf = self.surf_pts[row, col]
             to_put = self.flt_volume[row, col-NR:col+NR, surf-BK:surf+IN]
             if to_put.shape == to_return_x[c].shape:
-                # randomly flip half of the data
-                if(np.random.randint(10) % 2 == 0):
+                if (np.random.randint(10) % 2 == 0):
                     to_put = np.flipud(to_put)
 
                 to_return_x[c] = to_put
-                to_return_y[c][0] = self.ground_truth[row, col]
-                to_return_y[c][1] = 1 - self.ground_truth[row, col]
+                to_return_y[c][0] = 1 - self.ground_truth[row, col] # non-ink
+                to_return_y[c][1] = self.ground_truth[row, col] # ink
+
             c += 1
+
+        self.current_spot = end
+        return (to_return_x, to_return_y)
+
+
+
+    def next_batch_3d(self, batch_size):
+        """Returns 'batch_size' 3D training samples"""
+        #Use self.DEPTH to create 3D data. Also, use data from reverse side of fragment
+
+        width = 2*self.NR
+        height = self.C_IN + self.C_BACK
+        depth = self.DEPTH * 2
+        to_return_x = np.zeros((batch_size, width, height, depth), dtype=np.float32)
+        to_return_y = np.zeros((batch_size, 2), dtype=np.float32)
+
+        # if everything has been trained on, loop back
+        if(self.current_spot + batch_size > self.total_vects):
+            self.current_spot = 0
+            np.random.shuffle(self.train_inds)
+
+        start = self.current_spot
+        end = start + batch_size / 2 # using the reverse side halves the required indeces
+
+        batch_inds = self.train_inds[start:end]
+        c = 0
+        NR = self.NR
+        IN = self.C_IN
+        BK = self.C_BACK
+        DP = self.DEPTH
+        for ind in batch_inds:
+            row,col = self.ind_to_coord(ind)
+            surf = self.surf_pts[row, col]
+            to_put = self.flt_volume[row-DP:row+DP, col-NR:col+NR, surf-BK:surf+IN]
+            if to_put.shape == to_return_x[c].shape:
+                to_return_x[c] = to_put
+                to_return_y[c][0] = 1 - self.ground_truth[row, col] # non-ink
+                to_return_y[c][1] = self.ground_truth[row, col] # ink
+
+            # add the reverse side
+            rev_surf = self.surf_pts_reverse[row, col]
+            to_put_rev = self.flt_volume[row-DP:row+DP, col-NR:col+NR, rev_surf-IN:rev_surf+BK]
+            np.flip(to_put_rev, axis=2)
+            if to_put_rev.shape == to_return_x[c+1].shape:
+                to_return_x[c+1] = to_put
+                to_return_y[c+1][0] = 1.00 # all points on reverse side are 100% non-ink
+                to_return_y[c+1][1] = .00 # and 0% ink
+
+            c += 2
 
         self.current_spot = end
         return (to_return_x, to_return_y)
@@ -168,22 +220,24 @@ class TrainVol(object):
 
     def find_frag_surf_at_thresh(self, thresh):
         maxes = np.max(self.raw_volume, axis=2)
-        frag_mask = np.zeros(maxes.shape, dtype=np.uint16)
-        surf_pts = np.zeros(maxes.shape, dtype=np.int)
+        self.frag_mask = np.zeros(maxes.shape, dtype=np.uint16)
+        self.surf_pts = np.zeros(maxes.shape, dtype=np.int)
+        self.surf_pts_reverse = np.zeros(maxes.shape, dtype=np.int)
 
         n = self.fragment_buffer  # neighborhood to verify
 
         for i in range(n, self.raw_volume.shape[0]-n-1):
             for j in range(n, self.raw_volume.shape[1]-n-1):
+                # if points in the point's neighborhood exceed the threshold
                 if np.min(maxes[i-n:i+n+1, j-n:j+n+1]) > thresh:
-                    frag_mask[i,j] = 1
-                    vector = self.raw_volume[i,j]
-                    surf_pts[i,j] = np.where(vector > thresh)[0][0]
+                    # mark it as a point on the fragment
+                    self.frag_mask[i,j] = 1
+                    surface_vect = np.where(self.raw_volume[i,j] > thresh)[0]
+                    # store the surface's "start point"
+                    self.surf_pts[i,j] = surface_vect[0]
+                    # and the surface's "end point"
+                    self.surf_pts_reverse[i,j] = surface_vect[-1]
 
-        self.frag_mask = frag_mask
-        self.surf_pts = surf_pts
-
-    
 
 
     def ind_to_coord(self, ind):
