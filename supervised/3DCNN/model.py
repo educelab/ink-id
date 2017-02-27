33706576e844337a0b668dc3e192135deac8be1c
Jack Bandy <jgba225@g.uky.edu>
2017-02-27 17:50:10 -0500

    Corrected conv5 to connect to conv4 instead of conv3

--- supervised/3DCNN/model.py
+++ supervised/3DCNN/model.py
@@ -5,11 +5,11 @@ import pdb
 
 def buildModel(x, y, args):
     x = tf.reshape(x, [-1, args["x_Dimension"], args["y_Dimension"], args["z_Dimension"], 1])
-    conv1 = slim.batch_norm(slim.convolution(x, 8, [3,3,3], stride=2))
-    conv2 = slim.batch_norm(slim.convolution(conv1, 16, [3,3,3], stride=2))
-    conv3 = slim.batch_norm(slim.convolution(conv2, 32, [3,3,3], stride=2))
-    conv4 = slim.batch_norm(slim.convolution(conv3, 64, [3,3,3], stride=2))
-    conv5 = slim.batch_norm(slim.convolution(conv3, 128, [3,3,3], stride=2))
+    conv1 = slim.batch_norm(slim.convolution(x, 8, args["receptiveField"], stride=2))
+    conv2 = slim.batch_norm(slim.convolution(conv1, 16, args["receptiveField"], stride=2))
+    conv3 = slim.batch_norm(slim.convolution(conv2, 32, args["receptiveField"], stride=2))
+    conv4 = slim.batch_norm(slim.convolution(conv3, 64, args["receptiveField"], stride=2))
+    conv5 = slim.batch_norm(slim.convolution(conv4, 128, args["receptiveField"], stride=2))
 
     pred = tf.nn.dropout(slim.fully_connected(slim.flatten(conv5), args["n_Classes"], activation_fn=None), args["dropout"])
     loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=pred, labels=y))
