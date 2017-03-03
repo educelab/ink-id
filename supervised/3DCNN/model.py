2ed814f2924aea983b01c75a4d2465d8351b1c97
Jack Bandy <jgba225@g.uky.edu>
2017-03-03 15:21:13 -0500

    Added another convolutional layer for a total of 6 convolutional layers and 2 fully-connected layers

--- supervised/3DCNN/model.py
+++ supervised/3DCNN/model.py
@@ -10,8 +10,9 @@ def buildModel(x, y, args):
     conv3 = slim.batch_norm(slim.convolution(conv2, 32, args["receptiveField"], stride=2))
     conv4 = slim.batch_norm(slim.convolution(conv3, 64, args["receptiveField"], stride=2))
     conv5 = slim.batch_norm(slim.convolution(conv4, 128, args["receptiveField"], stride=2))
+    conv6 = slim.batch_norm(slim.convolution(conv5, 256, args["receptiveField"], stride=2))
 
-    fc1 = tf.nn.dropout(slim.fully_connected(slim.flatten(conv5), 256), args["dropout"])
+    fc1 = tf.nn.dropout(slim.fully_connected(slim.flatten(conv6), 512), args["dropout"])
     net = tf.nn.dropout(slim.fully_connected(fc1, args["n_Classes"], activation_fn=None), args["dropout"])
     loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits=net, labels=y))
 
