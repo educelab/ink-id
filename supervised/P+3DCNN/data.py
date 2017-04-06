22b88ff6b102d9f55ef0b73dbb854e54d2bc298d
kmwe236 <kmwe236>
2017-04-06 12:33:57 -0400

    changed code to multipower-single-channel --> still need to clean everything up

--- supervised/P+3DCNN/data.py
+++ supervised/P+3DCNN/data.py
@@ -56,7 +56,7 @@ class Volume:
         trainingSamples = []
         groundTruth = []
 
-        for i in range(args["numCubes"]):
+        for i in range(args["batchSize"]):
             xCoordinate, yCoordinate, zCoordinate, label_avg = ops.findRandomCoordinates(args, xBounds, yBounds, self.volume, self.groundTruth)
 
             spectralSamples = []
@@ -83,12 +83,81 @@ class Volume:
 
         return np.transpose(np.array(trainingSamples), (0, 2, 3, 4, 1)), np.array(groundTruth)
 
+    def getTrainingSample_MultipowerSingleChannel(self, args, testSet=False, bounds=0):
+        # bounds parameters: 0=TOP || 1=RIGHT || 2=BOTTOM || 3=LEFT
+        if testSet:
+            xBounds, yBounds = ops.bounds(args, [self.volume.shape[1], self.volume.shape[2]], (bounds+2)%4)
+        else:
+            xBounds, yBounds = ops.bounds(args, [self.volume.shape[1], self.volume.shape[2]], bounds)
+
+        trainingSamples = []
+        groundTruth = []
+
+        while len(trainingSamples) < args["batchSize"]:
+            xCoordinate, yCoordinate, zCoordinate, label_avg = ops.findRandomCoordinates(args, xBounds, yBounds, self.volume, self.groundTruth)
+
+            if ops.edge(xCoordinate, args["x_Dimension"], self.volume.shape[1]) or ops.edge(yCoordinate, args["y_Dimension"], self.volume.shape[2]):
+                for j in range(self.volume.shape[0]):
+                    sample = ops.findEdgeSubVolume(args, xCoordinate, yCoordinate, self.volume, j)
+                    trainingSamples.append(sample)
+            else:
+                for j in range(self.volume.shape[0]):
+                    trainingSamples.append(self.volume[j, xCoordinate:xCoordinate+args["x_Dimension"], \
+                                yCoordinate:yCoordinate+args["y_Dimension"], zCoordinate:zCoordinate+args["z_Dimension"]])
+
+            no_ink = len(np.where(self.groundTruth[xCoordinate:xCoordinate+args["x_Dimension"], \
+                        yCoordinate:yCoordinate+args["y_Dimension"]] == 0)[0])
+            ink = len(np.where(self.groundTruth[xCoordinate:xCoordinate+args["x_Dimension"], \
+                        yCoordinate:yCoordinate+args["y_Dimension"]] == 255)[0])
+
+            classification = np.argmax([no_ink, ink])
+            gt = [0,0]
+            gt[classification] = 1.0
+            for j in range(self.volume.shape[0]):
+                groundTruth.append(gt)
+
+        return np.reshape(np.array(trainingSamples), (args["batchSize"], args["x_Dimension"], args["y_Dimension"], args["z_Dimension"], args["numChannels"])), np.array(groundTruth)
+
     def getPredictionSample(self, args, startingCoordinates):
         xCoordinate = startingCoordinates[0]
         yCoordinate = startingCoordinates[1]
         zCoordinate = startingCoordinates[2]
 
         predictionSamples = np.zeros((args["predictBatchSize"], args["numChannels"], args["x_Dimension"], args["y_Dimension"], args["z_Dimension"]), dtype=np.float32)
+        coordinates = np.zeros((args["predictBatchSize"], 2), dtype=np.int)
+        count = 0
+        while count < args["predictBatchSize"]:
+            if xCoordinate > self.volume.shape[1]:
+                xCoordinate = 0
+                yCoordinate += args["stride"]
+            if yCoordinate > self.volume.shape[2]:
+                # yCoordinate = 0
+                break
+
+            spectralSamples = []
+            if ops.edge(xCoordinate, args["x_Dimension"], self.volume.shape[1]) or ops.edge(yCoordinate, args["y_Dimension"], self.volume.shape[2]):
+                for i in range(self.volume.shape[0]):
+                    sample = ops.findEdgeSubVolume(args, xCoordinate, yCoordinate, self.volume, i)
+                    spectralSamples.append(sample)
+            else:
+                for i in range(self.volume.shape[0]):
+                    spectralSamples.append(self.volume[i, xCoordinate:xCoordinate+args["x_Dimension"], \
+                            yCoordinate:yCoordinate+args["y_Dimension"], zCoordinate:zCoordinate+args["z_Dimension"]])
+
+            predictionSamples[count,:,:,:,:] = spectralSamples
+            coordinates[count] = [xCoordinate, yCoordinate]
+
+            xCoordinate += args["stride"]
+            count += 1
+
+        return np.transpose(predictionSamples, (0,2,3,4,1)), coordinates, [xCoordinate, yCoordinate, zCoordinate]
+
+    def getPredictionSample_MultipowerSingleChannel(self, args, startingCoordinates):
+        xCoordinate = startingCoordinates[0]
+        yCoordinate = startingCoordinates[1]
+        zCoordinate = startingCoordinates[2]
+
+        predictionSamples = np.zeros((args["predictBatchSize"], args["numVolumes"], args["x_Dimension"], args["y_Dimension"], args["z_Dimension"]), dtype=np.float32)
         coordinates = np.zeros((args["predictBatchSize"], 2), dtype=np.int)
         count = 0
         while count < args["predictBatchSize"]:
@@ -125,6 +194,11 @@ class Volume:
     def emptyPredictionVolume(self, args):
         self.predictionImage = np.zeros((int(self.volume.shape[1]/args["stride"]), int(self.volume.shape[2]/args["stride"])), dtype=np.uint8)
 
+    def initMultiplePredictionImages(self, args):
+        self.predictionImages = []
+        for i in range(self.volume.shape[0]):
+            self.predictionImages.append(np.zeros((int(self.volume.shape[1]/args["stride"]), int(self.volume.shape[2]/args["stride"])), dtype=np.uint8))
+
     def reconstruct(self, args, samples, coordinates):
         # reconstruct prediction volume one prediction sample at a time
         for i in range(coordinates.shape[0]):
@@ -134,5 +208,19 @@ class Volume:
                 except:
                     pass
 
+    def reconstruct_MulipowerSingleChannel(self, args, samples, coordinates):
+        # reconstruct prediction volume one prediction sample at a time
+        for i in range(len(self.predictionImages)):
+            for j in range(coordinates.shape[0]):
+                if np.argmax(samples[i][j,:]) == 1:
+                    try:
+                        self.predictionImages[i][int(coordinates[j,0]/args["stride"]), int(coordinates[j,1]/args["stride"])] = 255
+                    except:
+                        pass
+
     def savePredictionImage(self, args, epoch):
         cv2.imwrite(args["savePredictionPath"] + str(epoch) + ".png", self.predictionImage)
+
+    def savePredictionImages(self, args, epoch):
+        for i in range(len(self.predictionImages)):
+            cv2.imwrite(args["savePredictionPath"] + "volume-" + str(i) + "-epoch-" + str(epoch) + ".png", self.predictionImage)
--- supervised/P+3DCNN/main.py
+++ supervised/P+3DCNN/main.py
@@ -7,35 +7,29 @@ import sys
 import data
 import model
 
-# if sys.argv[5] == "true":
-#     mulitpower = True
-# else:
-#     multipower = False
-
 args = {
-    "trainingDataPath": "/home/volcart/volumes/packages/CarbonPhantom-Feb2017.volpkg/paths/20170221130948/layered/registered/layers/full-layers/after-rotate/",
-    "mulitpower": "false",
-    "groundTruthFile": "/home/volcart/volumes/packages/CarbonPhantom-Feb2017.volpkg/paths/20170221130948/layered/registered/ground-truth/GroundTruth-CarbonInk.png",
-    "savePredictionPath": "/home/volcart/supervised-results/CarbonPhantom-Feb2017/",
+    "trainingDataPath": "/home/volcart/volumes/packages/CarbonPhantom_MP_2017.volpkg/paths/all-cols/",
+    "mulitpower": "true",
+    "groundTruthFile": "/home/volcart/volumes/packages/CarbonPhantom_MP_2017.volpkg/paths/all-cols/GroundTruth-CarbonInk.png",
+    "savePredictionPath": "/home/volcart/supervised-results/multipower-single-channel/",
     "saveModelPath": "/home/volcart/prelim-InkDetection/src/results/models/",
     "numChannels": 1,
+    "numVolumes": 6,
     "cropX_low": int(sys.argv[1]),
     "cropX_high": int(sys.argv[2]),
     "cropY_low": int(sys.argv[3]),
     "cropY_high": int(sys.argv[4]),
     "x_Dimension": 25,
     "y_Dimension": 25,
-    "z_Dimension": 17,
+    "z_Dimension": 71,
     "stride": 1,
-    "numCubes": 500,
-    "predictBatchSize": 500,
+    "predictBatchSize": 120, # NOTE: for multipower single channel, this must be a multiple of numVolumes
     "n_Classes": 2,
     "learningRate": 0.0001,
-    "batchSize": 30,
+    "batchSize": 36,
     "dropout": 0.75,
     "trainingIterations": 30001,
-    "grabNewSamples": 40,
-    "predictStep": 2000,
+    "predictStep": 1000,
     "displayStep": 10,
     "singleScanPath": "/home/volcart/volumes/packages/CarbonPhantom-Feb2017.volpkg/paths/20170221130948/layered/registered/layers/full-layers/after-rotate/"
 }
@@ -57,40 +51,48 @@ with tf.Session() as sess:
     sess.run(init)
     # saver.restore(sess, args["saveModelPath"]+"model-epoch-4000.ckpt")
     epoch = 0
-    avgOutputVolume = []
     while epoch < args["trainingIterations"]:
-        if epoch % args["grabNewSamples"] == 0:
-            trainingSamples, groundTruth = volume.getTrainingSample(args)
+        # if epoch % args["grabNewSamples"] == 0:
+        #     # trainingSamples, groundTruth = volume.getTrainingSample(args)
+        #     trainingSamples, groundTruth = volume.getTrainingSample_MultipowerSingleChannel(args)
+        #
+        # pdb.set_trace()
+        # randomBatch = np.random.randint(trainingSamples.shape[0] - args["batchSize"])
+        # batchX = trainingSamples[randomBatch:randomBatch+args["batchSize"]]
+        # batchY = groundTruth[randomBatch:randomBatch+args["batchSize"]]
 
-        randomBatch = np.random.randint(trainingSamples.shape[0] - args["batchSize"])
-        batchX = trainingSamples[randomBatch:randomBatch+args["batchSize"]]
-        batchY = groundTruth[randomBatch:randomBatch+args["batchSize"]]
+        # sess.run(optimizer, feed_dict={x: batchX, y: batchY, keep_prob: args["dropout"]})
 
-        sess.run(optimizer, feed_dict={x: batchX, y: batchY, keep_prob: args["dropout"]})
+        trainingSamples, groundTruth = volume.getTrainingSample_MultipowerSingleChannel(args)
+        sess.run(optimizer, feed_dict={x: trainingSamples, y: groundTruth, keep_prob: args["dropout"]})
 
         if epoch % args["displayStep"] == 0:
-            acc = sess.run(accuracy, feed_dict={x: batchX, y: batchY, keep_prob: 1.0})
-            evaluatedLoss = sess.run(loss, feed_dict={x: batchX, y: batchY, keep_prob: 1.0})
+            acc, evaluatedLoss = sess.run([accuracy, loss], feed_dict={x: trainingSamples, y: groundTruth, keep_prob: 1.0})
             print("Epoch: " + str(epoch) + "  Loss: " + str(np.mean(evaluatedLoss)) + "  Acc: " + str(np.mean(acc)))
 
         if epoch % args["predictStep"] == 0 and epoch > 0:
-            volume.emptyPredictionVolume(args)
+            # volume.emptyPredictionVolume(args)
+            volume.initMultiplePredictionImages(args)
             total_num_predictions = volume.totalPredictions(args)
 
             startingCoordinates = [0,0,0]
-            predictionSamples, coordinates, nextCoordinates = volume.getPredictionSample(args, startingCoordinates)
+            # predictionSamples, coordinates, nextCoordinates = volume.getPredictionSample(args, startingCoordinates)
+            predictionSamples, coordinates, nextCoordinates = volume.getPredictionSample_MultipowerSingleChannel(args, startingCoordinates)
 
             count = 1
-            while ((count-1)*args["predictBatchSize"]) < total_num_predictions: # TODO what about the special case where the volume is a perfect multiple of the numCubes?
+            pdb.set_trace()
+            while ((count-1)*args["predictBatchSize"]) < total_num_predictions:
                 print("Predicting cubes {} of {}".format((count * args["predictBatchSize"]), total_num_predictions))
-                predictionValues = sess.run(pred, feed_dict={x: predictionSamples, keep_prob: 1.0})
-                volume.reconstruct(args, predictionValues, coordinates)
-                predictionSamples, coordinates, nextCoordinates = volume.getPredictionSample(args, nextCoordinates)
+                predictionValues = []
+                for i in range(predictionSamples.shape[4]):
+                    predictionValues.append(sess.run(pred, feed_dict={x: predictionSamples[:,:,:,:,i:i+1], keep_prob: 1.0}))
+                volume.reconstruct_MulipowerSingleChannel(args, predictionValues, coordinates)
+                # predictionSamples, coordinates, nextCoordinates = volume.getPredictionSample(args, nextCoordinates)
+                predictionSamples, coordinates, nextCoordinates = volume.getPredictionSample_MultipowerSingleChannel(args, startingCoordinates)
                 count += 1
-                volume.savePredictionImage(args, epoch)
+                volume.savePredictionImages(args, epoch)
             volume.savePredictionImage(args, epoch)
             # save_path = saver.save(sess, args["saveModelPath"]+"model-epoch-"+str(epoch)+".ckpt")
             # print("Model saved in file: %s" % save_path)
-
 
         epoch = epoch + 1
