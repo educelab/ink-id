Added histograms, began fixing batch normalization for test set (WIP)

supervised/3DCNN/main.py
@@ -34,7 +34,7 @@ args = {
     "use_multitask_training": False,
     "shallow_learning_rate":.001,
     "learning_rate": .0001,
-    "batch_size": 24,
+    "batch_size": 50,
     "prediction_batch_size": 500,
     "filter_size" : [3,3,3],
     "dropout": 0.5,
@@ -43,11 +43,12 @@ args = {
     "training_epochs": 2,
     "n_classes": 2,
     "pos_weight": .5,
+    "batch_norm_momentum": .99,
 
     ### Data configuration ###
-    "wobble_volume" : True,
+    "wobble_volume" : False,
     "wobble_step" : 1000,
-    "wobble_max_degrees" : 3,
+    "wobble_max_degrees" : 2,
     "num_test_cubes" : 500,
     "add_random" : False,
     "random_step" : 10, # one in every randomStep non-ink samples will be a random brick
@@ -65,7 +66,7 @@ args = {
     "restrict_surface": True,
 
     ### Output configuration ###
-    "predict_step": 5000, # make a prediction every x steps
+    "predict_step": 1000, # make a prediction every x steps
     "overlap_step": 4, # during prediction, predict on one sample for each _ by _ voxel square
     "display_step": 50, # output stats every x steps
     "predict_depth" : 1,
@@ -81,13 +82,13 @@ args = {
 x = tf.placeholder(tf.float32, [None, args["x_Dimension"], args["y_Dimension"], args["z_Dimension"]])
 y = tf.placeholder(tf.float32, [None, args["n_classes"]])
 drop_rate = tf.placeholder(tf.float32)
-
+train_flag = tf.placeholder(tf.bool)
 
 if args["use_multitask_training"]:
-    pred, shallow_loss, loss = model.buildMultitaskModel(x, y, drop_rate, args)
+    pred, shallow_loss, loss = model.buildMultitaskModel(x, y, drop_rate, args, train_flag)
     shallow_optimizer = tf.train.AdamOptimizer(learning_rate=args["shallow_learning_rate"]).minimize(shallow_loss)
 else:
-    pred, loss = model.buildModel(x, y, drop_rate, args)
+    pred, loss = model.buildModel(x, y, drop_rate, args, train_flag)
 
 optimizer = tf.train.AdamOptimizer(learning_rate=args["learning_rate"]).minimize(loss)
 correct_pred = tf.equal(tf.argmax(pred, 1), tf.argmax(y, 1))
@@ -95,6 +96,7 @@ accuracy = tf.reduce_mean(tf.cast(correct_pred, tf.float32))
 false_positives = tf.equal(tf.argmax(y,1) + 1, tf.argmax(pred, 1))
 false_positive_rate = tf.reduce_mean(tf.cast(false_positives, tf.float32))
 tf.summary.scalar('accuracy', accuracy)
+tf.summary.histogram('prediction_values', pred[:,1])
 tf.summary.scalar('xentropy-loss', loss)
 if args["use_multitask_training"]:
     tf.summary.scalar('xentropy-shallow-loss', loss)
@@ -143,17 +145,17 @@ with tf.Session() as sess:
 
             batchX, batchY, epoch = volume.getTrainingBatch(args)
             if args["use_multitask_training"]:
-                summary, _, _ = sess.run([merged, optimizer, shallow_optimizer], feed_dict={x: batchX, y: batchY, drop_rate:args["dropout"]})
+                summary, _, _ = sess.run([merged, optimizer, shallow_optimizer], feed_dict={x: batchX, y: batchY, drop_rate:args["dropout"], train_flag:True})
             else:
-                summary, _ = sess.run([merged, optimizer], feed_dict={x: batchX, y: batchY, drop_rate:args["dropout"]})
+                summary, _ = sess.run([merged, optimizer], feed_dict={x: batchX, y: batchY, drop_rate:args["dropout"], train_flag:True})
 
             train_writer.add_summary(summary, iteration)
 
             if iteration % args["display_step"] == 0:
                 train_acc, train_loss, train_preds, train_summary = \
-                    sess.run([accuracy, loss, pred, merged], feed_dict={x: batchX, y: batchY, drop_rate: 0.0})
+                    sess.run([accuracy, loss, pred, merged], feed_dict={x: batchX, y: batchY, drop_rate: 0.0, train_flag:False})
                 test_acc, test_loss, test_preds, test_summary = \
-                    sess.run([accuracy, loss, pred, merged], feed_dict={x: testX, y: testY, drop_rate:0.0})
+                    sess.run([accuracy, loss, pred, merged], feed_dict={x: testX, y: testY, drop_rate:0.0, train_flag:False})
                 train_prec = precision_score(np.argmax(batchY, 1), np.argmax(train_preds, 1))
                 test_prec = precision_score(np.argmax(testY, 1), np.argmax(test_preds, 1))
 
@@ -191,7 +193,7 @@ with tf.Session() as sess:
                     if (count % int(total_prediction_batches / 10) == 0):
                         #update UI at 10% intervals
                         print("Predicting cubes {} of {}".format((count * args["prediction_batch_size"]), total_predictions))
-                    predictionValues = sess.run(pred, feed_dict={x: predictionSamples, drop_rate: 1.0})
+                    predictionValues = sess.run(pred, feed_dict={x: predictionSamples, drop_rate: 1.0, train_flag:False})
                     volume.reconstruct3D(args, predictionValues, coordinates)
                     predictionSamples, coordinates, nextCoordinates = volume.getPredictionSample3D(args, nextCoordinates)
                     count += 1
@@ -222,7 +224,7 @@ with tf.Session() as sess:
             if (count % int(total_prediction_batches / 10) == 0):
                 #update UI at 10% intervals
                 print("Predicting cubes {} of {}".format((count * args["prediction_batch_size"]), total_predictions))
-            predictionValues = sess.run(pred, feed_dict={x: predictionSamples, drop_rate: 1.0})
+            predictionValues = sess.run(pred, feed_dict={x: predictionSamples, drop_rate: 1.0, train_flag:False})
             volume.reconstruct3D(args, predictionValues, coordinates)
             predictionSamples, coordinates, nextCoordinates = volume.getPredictionSample3D(args, nextCoordinates)
             count += 1

supervised/3DCNN/model.py
@@ -5,19 +5,19 @@ from tensorflow import layers
 import pdb
 
 
-def buildModel(x, y, drop_rate, args):
+def buildModel(x, y, drop_rate, args, training_flag):
     x = (tf.reshape(x, [-1, args["x_Dimension"], args["y_Dimension"], args["z_Dimension"], 1]))
-    conv1 = slim.batch_norm(slim.convolution(x, args["neurons"][0], [3, 3, 3], stride=[2,2,2], padding='valid'))
-    conv2 = slim.batch_norm(slim.convolution(conv1, args["neurons"][1], [3, 3, 3], stride=[2,2,2], padding='valid'))
-    conv3 = slim.batch_norm(slim.convolution(conv2, args["neurons"][2], [3, 3, 3], stride=[2,2,2], padding='valid'))
-    conv4 = slim.batch_norm(slim.convolution(conv3, args["neurons"][3], [3, 3, 3], stride=[2,2,2], padding='valid'))
+    conv1 = layers.batch_normalization(slim.convolution(x, args["neurons"][0], [3, 3, 3], stride=[2,2,2], padding='valid'), training=training_flag, scale=False, axis=4, momentum=args["batch_norm_momentum"])
+    conv2 = layers.batch_normalization(slim.convolution(conv1, args["neurons"][1], [3, 3, 3], stride=[2,2,2], padding='valid'), training=training_flag, scale=False, axis=4, momentum=args["batch_norm_momentum"])
+    conv3 = layers.batch_normalization(slim.convolution(conv2, args["neurons"][2], [3, 3, 3], stride=[2,2,2], padding='valid'), training=training_flag, scale=False, axis=4, momentum=args["batch_norm_momentum"])
+    conv4 = layers.batch_normalization(slim.convolution(conv3, args["neurons"][3], [3, 3, 3], stride=[2,2,2], padding='valid'), training=training_flag, scale=False, axis=4, momentum=args["batch_norm_momentum"])
 
     net = layers.dropout(slim.fully_connected(slim.flatten(conv4), args["n_classes"], activation_fn=None), rate=drop_rate)
 
-    #loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y, logits=net))
-    targets_1d = y[:,1]
-    logits_1d = net[:,1]
-    loss = tf.reduce_mean(tf.nn.weighted_cross_entropy_with_logits(logits=logits_1d, targets=targets_1d, pos_weight=args["pos_weight"]))
+    loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y, logits=net))
+    #targets_1d = y[:,1]
+    #logits_1d = net[:,1]
+    #loss = tf.reduce_mean(tf.nn.weighted_cross_entropy_with_logits(logits=logits_1d, targets=targets_1d, pos_weight=args["pos_weight"]))
 
     return tf.nn.softmax(net), loss
 