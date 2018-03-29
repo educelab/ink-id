164c80c74f2320a83370610ae02d3cbdb2b307e1
Stephen Parsons <stephen.r.parsons@gmail.com>
2018-03-29 12:44:21 -0400

    remove some old code

--- inkid/volumes/VolumeSet.py
+++ inkid/volumes/VolumeSet.py
@@ -161,6 +161,14 @@ class VolumeSet:
                                                            augment_samples)
 
     
+    def coordinate_pool_generator(self, grid_spacing, perform_shuffle):
+        def generator():
+            for volume in self.volume_set:
+                for coordinate in volume.yield_coordinates(grid_spacing, perform_shuffle):
+                    yield coordinate
+        return generator
+
+
     def is_on_surface(self, vol_id, xy_coordinate):
         return self.volume_set[vol_id].is_on_surface(xy_coordinate)
     
@@ -169,113 +177,9 @@ class VolumeSet:
         return tf.py_func(self.is_on_surface, [vol_id, xy_coordinate], [tf.bool])
 
         
-    def getTestBatch(self, args):
-        """DEPRECATED"""
-        # gather testing samples from other volumes
-        trainingSamples = np.zeros(
-            (args["evaluation_batch_size"],
-             args["subvolume_shape"][0],
-             args["subvolume_shape"][1],
-             args["subvolume_shape"][2]
-            ),
-            dtype=np.float32
-        )
-        groundTruth = np.zeros(
-            (args["evaluation_batch_size"], 2),
-            dtype=np.float32
-        )
-
-        if self.n_test_volumes == 1:
-            # all samples come from the same volume
-            volume_samples, volume_truth = self.volume_set[self.test_volume_indices[0]].getTestBatch(args, args["evaluation_batch_size"])
-            trainingSamples[:] = volume_samples
-            groundTruth[:] = volume_truth
-
-        else: #TODO validate this scheme
-            samples_per_volume = int(args["evaluation_batch_size"] / self.n_test_volumes)
-            for i in range(self.n_test_volumes):
-                volume_index = self.test_volume_indices[i]
-
-                start = i*samples_per_volume
-                end = (i+1)*samples_per_volume
-                if i == self.n_test_volumes-1: # make sure to 'fill up' all the slots
-                    end = args["evaluation_batch_size"]
-
-                volume_samples, volume_truth = self.volume_set[volume_index].getTestBatch(args, end-start)
-                trainingSamples[start:end] = volume_samples
-                groundTruth[start:end] = volume_truth
-
-        return trainingSamples, groundTruth
-
-    def getTrainingBatch(self, args):
-        """DEPRECATED"""
-        # gather training samples from other volumes
-        # should be as simple as getting batches from each volume and combining them
-        trainingSamples = np.zeros(
-            (args["training_batch_size"],
-             args["subvolume_shape"][0],
-             args["subvolume_shape"][1],
-             args["subvolume_shape"][2]
-            ),
-            dtype=np.float32)
-        groundTruth = np.zeros(
-            (args["training_batch_size"], 2),
-            dtype=np.float32
-        )
-        samples_per_volume = int(args["training_batch_size"] / self.n_train_volumes)
-        for i in range(self.n_train_volumes):
-            volume_index = self.train_volume_indices[i]
-            start = i*samples_per_volume
-            end = (i+1)*samples_per_volume
-            if i == self.n_train_volumes-1: # make sure to 'fill up' all the slots
-                end = args["training_batch_size"]
-
-            volume_samples, volume_truth, volume_epoch = self.volume_set[volume_index].getTrainingBatch(args, n_samples=(end-start))
-            trainingSamples[start:end] = volume_samples
-            groundTruth[start:end] = volume_truth
-
-        #TODO return actual epoch instead of the last volume's epoch
-        return trainingSamples, groundTruth, volume_epoch
-
-
-
-    def getPredictionBatch(self, args, starting_coordinates):
-        # predict on one volume at a time
-        # batches should be from one volume at a time
-        self.current_prediction_total_batches = int(self.volume_set[self.current_prediction_volume].totalPredictions(args, args["prediction_overlap_step"]) / args["prediction_batch_size"])
-
-        if self.current_prediction_batch < self.current_prediction_total_batches:
-            # case 1: stay in the current volume
-            samples, coordinates, nextCoordinates = self.volume_set[self.current_prediction_volume].getPredictionSample3D(args, starting_coordinates, overlap_step=args['prediction_overlap_step'])
-            self.current_prediction_batch += 1
-            if self.current_prediction_batch % int(self.current_prediction_total_batches / 10) == 0:
-                print("\tPredicting batch {}/{}...".format(self.current_prediction_batch, self.current_prediction_total_batches))
-
-        elif self.current_prediction_volume is not self.predict_volume_indices[-1]:
-            # case 2: finished one volume, and it was not the last volume to predict on
-            print("Finished predictions on volume {}...".format(self.current_prediction_volume))
-            self.current_prediction_volume += 1
-            while not args["volumes"][self.current_prediction_volume]['make_prediction']:
-                self.current_prediction_volume += 1
-            v_olap = args["prediction_overlap_step"]
-            self.current_prediction_batch = 0
-            starting_coordinates = [0,0,0]
-            samples, coordinates, nextCoordinates = self.volume_set[self.current_prediction_volume].getPredictionSample3D(args, starting_coordinates, overlap_step=v_olap)
-            print("\nBeginning predictions on volume {}...".format(self.current_prediction_volume))
-
-        else:
-            # case 3: finished volume, no other volume to predict on
-            samples, coordinates, nextCoordinates = None, None, None
-            self.current_prediction_volume = self.predict_volume_indices[0]
-            self.current_prediction_batch = 0
-
-        return samples, coordinates, nextCoordinates
-
-
     def reconstruct(self, args, prediction_values, coordinates):
         # fill in the prediction images
         self.volume_set[self.current_prediction_volume].reconstruct3D(args, prediction_values, coordinates)
-
 
 
     def saveAllPredictions(self, args, iteration):
@@ -284,14 +188,7 @@ class VolumeSet:
             self.volume_set[i].savePrediction3D(args, iteration)
 
 
-
     def saveAllPredictionMetrics(self, args, iteration, minutes):
         # save metrics on performance
         for i in self.predict_volume_indices:
             self.volume_set[i].savePredictionMetrics(args, iteration, minutes)
-
-
-
-    def wobbleVolumes(self, args):
-        for i in self.train_volume_indices:
-            self.volume_set[i].wobbleVolume(args)
