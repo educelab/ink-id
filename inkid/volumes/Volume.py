0f93af7721bc26913ecaca2d416a98c854bbcedb
Stephen Parsons <stephen.r.parsons@gmail.com>
2018-04-04 13:37:44 -0400

    remove sklearn dependency from Volume

--- inkid/volumes/Volume.py
+++ inkid/volumes/Volume.py
@@ -8,7 +8,7 @@ import time
 import imageio
 import numpy as np
 from PIL import Image
-from sklearn.metrics import confusion_matrix, recall_score, precision_score, f1_score
+# from sklearn.metrics import confusion_matrix, recall_score, precision_score, f1_score
 import tensorflow as tf
 
 from inkid import ops
@@ -322,48 +322,48 @@ class Volume:
 
     def savePredictionMetrics(self, args, iteration, minutes):
         print("\n\n\tMetrics for {}:".format(self.volume_args['name']))
-        all_confusion = confusion_matrix(self.all_truth, self.all_preds)
-        test_confusion = confusion_matrix(self.test_truth, self.test_preds)
-        all_confusion_norm = all_confusion.astype('float') / all_confusion.sum(axis=1)[:, np.newaxis]
-        test_confusion_norm = test_confusion.astype('float') / test_confusion.sum(axis=1)[:, np.newaxis]
-        print("\tNormalized confusion matrix for ALL points: \n{}".format(all_confusion_norm))
-        print("\tNormalized confusion matrix for TEST points: \n{}".format(test_confusion_norm))
+        # all_confusion = confusion_matrix(self.all_truth, self.all_preds)
+        # test_confusion = confusion_matrix(self.test_truth, self.test_preds)
+        # all_confusion_norm = all_confusion.astype('float') / all_confusion.sum(axis=1)[:, np.newaxis]
+        # test_confusion_norm = test_confusion.astype('float') / test_confusion.sum(axis=1)[:, np.newaxis]
+        # print("\tNormalized confusion matrix for ALL points: \n{}".format(all_confusion_norm))
+        # print("\tNormalized confusion matrix for TEST points: \n{}".format(test_confusion_norm))
 
-        #calculate metrics
-        all_precision = precision_score(self.all_truth, self.all_preds)
-        all_recall = recall_score(self.all_truth, self.all_preds)
-        all_f1 = f1_score(self.all_truth, self.all_preds)
-        test_precision = precision_score(self.test_truth, self.test_preds)
-        test_recall = recall_score(self.test_truth, self.test_preds)
-        test_f1 = f1_score(self.test_truth, self.test_preds)
+        # #calculate metrics
+        # all_precision = precision_score(self.all_truth, self.all_preds)
+        # all_recall = recall_score(self.all_truth, self.all_preds)
+        # all_f1 = f1_score(self.all_truth, self.all_preds)
+        # test_precision = precision_score(self.test_truth, self.test_preds)
+        # test_recall = recall_score(self.test_truth, self.test_preds)
+        # test_f1 = f1_score(self.test_truth, self.test_preds)
 
-        test_confusion_norm_list = [0,0,0,0]
-        all_confusion_norm_list = [0,0,0,0]
-        try:
-            test_confusion_norm_list = test_confusion_norm.reshape(4).tolist()
-            all_confusion_norm_list = all_confusion_norm.reshape(4).tolist()
-        except:
-            pass
-        #save results in csv
-        column_names = 'iteration, minutes, true positive papyrus, false positive ink, false positive papyrus, true positive ink, precision, recall, f1'
-        self.test_results_norm.append([iteration] + [minutes] + test_confusion_norm_list + [test_precision] + [test_recall] + [test_f1])
-        self.all_results_norm.append([iteration] + [minutes] + all_confusion_norm_list + [all_precision] + [all_recall] + [all_f1])
-        np.savetxt(self.output_path + "confusion-all.csv", self.all_results_norm, fmt='%1.4f', header=column_names, delimiter=',')
-        np.savetxt(self.output_path + "confusion-test.csv", self.test_results_norm, fmt='%1.4f', header=column_names, delimiter=',')
+        # test_confusion_norm_list = [0,0,0,0]
+        # all_confusion_norm_list = [0,0,0,0]
+        # try:
+        #     test_confusion_norm_list = test_confusion_norm.reshape(4).tolist()
+        #     all_confusion_norm_list = all_confusion_norm.reshape(4).tolist()
+        # except:
+        #     pass
+        # #save results in csv
+        # column_names = 'iteration, minutes, true positive papyrus, false positive ink, false positive papyrus, true positive ink, precision, recall, f1'
+        # self.test_results_norm.append([iteration] + [minutes] + test_confusion_norm_list + [test_precision] + [test_recall] + [test_f1])
+        # self.all_results_norm.append([iteration] + [minutes] + all_confusion_norm_list + [all_precision] + [all_recall] + [all_f1])
+        # np.savetxt(self.output_path + "confusion-all.csv", self.all_results_norm, fmt='%1.4f', header=column_names, delimiter=',')
+        # np.savetxt(self.output_path + "confusion-test.csv", self.test_results_norm, fmt='%1.4f', header=column_names, delimiter=',')
 
-        # save description of this training session
-        description = ""
-        for arg in sorted(args.keys()):
-            description += arg+": " + str(args[arg]) + "\n"
-        np.savetxt(self.output_path +'description.txt', [description], delimiter=' ', fmt="%s")
-        shutil.copy(os.path.join(os.path.dirname(inspect.getfile(inkid.model)), 'model.py'),
-                    self.output_path + 'network_model.txt')
+        # # save description of this training session
+        # description = ""
+        # for arg in sorted(args.keys()):
+        #     description += arg+": " + str(args[arg]) + "\n"
+        # np.savetxt(self.output_path +'description.txt', [description], delimiter=' ', fmt="%s")
+        # shutil.copy(os.path.join(os.path.dirname(inspect.getfile(inkid.model)), 'model.py'),
+        #             self.output_path + 'network_model.txt')
 
-        # zero-out predictions & images so next output is correct
-        self.all_truth = []
-        self.all_preds = []
-        self.test_truth = []
-        self.test_preds = []
+        # # zero-out predictions & images so next output is correct
+        # self.all_truth = []
+        # self.all_preds = []
+        # self.test_truth = []
+        # self.test_preds = []
 
 
 
