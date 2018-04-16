159f5de56e42cfd4ebc24a09b05118c4b9714892
Stephen Parsons <stephen.r.parsons@gmail.com>
2018-04-16 13:39:58 -0400

    Use 2 GPUs at once in sbatch script

--- scripts/slurm_train.sh
+++ scripts/slurm_train.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 #SBATCH --gres=gpu:2
-#SBATCH --job-name=lunateGridTraining
-#SBATCH --output=lunateGridTraining_%A_%a.out
-#SBATCH --array=0-9
 # In the above line, replace 1 with the number of GPUs you wish to reserve (1
 # or 2).
+#SBATCH --job-name=lunateGridTraining
+#SBATCH --output=lunateGridTraining_%A_%a.out
+#SBATCH --array=0-9%2
 
 echo "My SLURM_ARRAY_TASK_ID: " $SLURM_ARRAY_TASK_ID
 
