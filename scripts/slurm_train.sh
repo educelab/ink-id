80d83bf1ea7a79ae997aaf5adfdddbe6edf65585
Stephen Parsons <stephen.r.parsons@gmail.com>
2018-04-17 10:41:06 -0400

    Add comment to slurm_train script

--- scripts/slurm_train.sh
+++ scripts/slurm_train.sh
@@ -7,6 +7,8 @@
 #SBATCH --cpus-per-task=64
 #SBATCH --job-name=k_fold_training_and_prediction
 #SBATCH --output=k_fold_training_and_prediction_%A_%a.out
+
+# Array to iterate over, as well as (%) number of jobs to run at once.
 #SBATCH --array=0-9%2
 
 echo "My SLURM_ARRAY_TASK_ID: " $SLURM_ARRAY_TASK_ID
