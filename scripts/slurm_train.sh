fae61451eef4a14dfdfd9214e54966e19359b64c
Stephen Parsons <stephen.r.parsons@gmail.com>
2018-04-17 10:26:44 -0400

    Use 64 CPUs per sbatch task (2x training performance boost)

--- scripts/slurm_train.sh
+++ scripts/slurm_train.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+# Number of GPUs per job. For now is set to one so that we can run
+# multiple jobs at once, rather than one job with two GPUs.
 #SBATCH --gres=gpu:1
-# In the above line, replace 1 with the number of GPUs you wish to reserve (1
-# or 2) per job.
-#SBATCH --job-name=lunateGridTraining
-#SBATCH --output=lunateGridTraining_%A_%a.out
+
+#SBATCH --cpus-per-task=64
+#SBATCH --job-name=k_fold_training_and_prediction
+#SBATCH --output=k_fold_training_and_prediction_%A_%a.out
 #SBATCH --array=0-9%2
 
 echo "My SLURM_ARRAY_TASK_ID: " $SLURM_ARRAY_TASK_ID
