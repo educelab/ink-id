0d4167f77a0145fbe7e8c2da7c56a7c3ee496de9
Stephen Parsons <stephen.r.parsons@gmail.com>
2018-04-17 09:52:53 -0400

    Use just 1 GPU per job so 2 jobs can run concurrently. Closes #24

--- scripts/slurm_train.sh
+++ scripts/slurm_train.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#SBATCH --gres=gpu:2
+#SBATCH --gres=gpu:1
 # In the above line, replace 1 with the number of GPUs you wish to reserve (1
-# or 2).
+# or 2) per job.
 #SBATCH --job-name=lunateGridTraining
 #SBATCH --output=lunateGridTraining_%A_%a.out
 #SBATCH --array=0-9%2
