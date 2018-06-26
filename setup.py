8f4846a936457cf7c89121f39055a41332dda1b4
Stephen Parsons <srpa226@uky.edu>
2018-06-26 11:31:33 -0400

    More dependency shuffling

--- setup.py
+++ setup.py
@@ -1,28 +1,30 @@
 from setuptools import setup
 
-setup(name='inkid',
-      version='0.0.1',
-      description='Identify ink via machine learning.',
-      url='https://code.vis.uky.edu/seales-research/ink-id',
-      author='University of Kentucky',
-      license='MS-RSL',
-      packages=['inkid'],
-      install_requires=[
-          'imageio',
-          'jsmin',
-          'mathutils',
-          'matplotlib',
-          'Pillow',
-          'progressbar2',
-      ],
-      entry_points = {
-          'console_scripts': [
-              'inkid-train-and-predict = scripts.train_and_predict:main',
-          ],
-      },
-      extras_require = {
-          'tf': ['tensorflow>=1.5.0'],
-          'tf_gpu': ['tensorflow-gpu>=1.5.0'],
-      },
+setup(
+    name='inkid',
+    version='0.0.1',
+    description='Identify ink via machine learning.',
+    url='https://code.vis.uky.edu/seales-research/ink-id',
+    author='University of Kentucky',
+    license='MS-RSL',
+    packages=['inkid'],
+    install_requires=[
+        'configargparse',
+        'gitpython',
+        'imageio',
+        'jsmin',
+        'mathutils',
+        'matplotlib',
+        'Pillow',
+        'progressbar2',
+    ],
+    entry_points={
+        'console_scripts': [
+            'inkid-train-and-predict = scripts.train_and_predict:main',
+        ],
+    },
+    extras_require={
+        'tf': ['tensorflow>=1.5.0'],
+        'tf_gpu': ['tensorflow-gpu>=1.5.0'],
+    },
 )
-
