1010f04787b917ad7429b1ada822cd8fd9144277
Stephen Parsons <srpa226@uky.edu>
2020-04-29 11:36:18 -0400

    Dynamically calculate required linear layer size from convolution output shapes

--- inkid/model/model.py
+++ inkid/model/model.py
@@ -1,4 +1,24 @@
+import math
+from typing import Union
+
+import numpy as np
 import torch
+
+
+def conv_output_shape(input_shape, kernel_size: Union[int, tuple], stride: Union[int, tuple],
+                      padding: Union[int, tuple], dilation: Union[int, tuple] = 1):
+    dim = len(input_shape)
+    if isinstance(kernel_size, int):
+        kernel_size = (kernel_size,) * dim
+    if isinstance(stride, int):
+        stride = (stride,) * dim
+    if isinstance(padding, int):
+        padding = (padding,) * dim
+    if isinstance(dilation, int):
+        dilation = (dilation,) * dim
+    # https://pytorch.org/docs/stable/nn.html#torch.nn.Conv3d See "Shape:" section.
+    return tuple(math.floor((input_shape[d] + 2 * padding[d] - dilation[d] * (kernel_size[d] - 1) - 1) / stride[d] + 1)
+                 for d in range(dim))
 
 
 class Subvolume3DcnnModel(torch.nn.Module):
@@ -6,25 +26,34 @@ class Subvolume3DcnnModel(torch.nn.Module):
                  batch_norm_momentum, no_batch_norm, filters, output_neurons):
         super().__init__()
 
+        if pad_to_shape is not None:
+            input_shape = pad_to_shape
+        else:
+            input_shape = subvolume_shape
+
         self._batch_norm = not no_batch_norm
 
         self.conv1 = torch.nn.Conv3d(in_channels=1, out_channels=filters[0],
                                      kernel_size=3, stride=1, padding=1)
         self.batch_norm1 = torch.nn.BatchNorm3d(num_features=filters[0], momentum=batch_norm_momentum)
+        shape = conv_output_shape(input_shape, 3, 1, 1)
 
         self.conv2 = torch.nn.Conv3d(in_channels=filters[0], out_channels=filters[1],
                                      kernel_size=3, stride=2, padding=1)
         self.batch_norm2 = torch.nn.BatchNorm3d(num_features=filters[1], momentum=batch_norm_momentum)
+        shape = conv_output_shape(shape, 3, 2, 1)
 
         self.conv3 = torch.nn.Conv3d(in_channels=filters[1], out_channels=filters[2],
                                      kernel_size=3, stride=2, padding=1)
         self.batch_norm3 = torch.nn.BatchNorm3d(num_features=filters[2], momentum=batch_norm_momentum)
+        shape = conv_output_shape(shape, 3, 2, 1)
 
         self.conv4 = torch.nn.Conv3d(in_channels=filters[2], out_channels=filters[3],
                                      kernel_size=3, stride=2, padding=1)
         self.batch_norm4 = torch.nn.BatchNorm3d(num_features=filters[3], momentum=batch_norm_momentum)
+        shape = conv_output_shape(shape, 3, 2, 1)
 
-        self.fc = torch.nn.Linear(filters[3] * 216, output_neurons)  # TODO change this input size based on padding
+        self.fc = torch.nn.Linear(filters[3] * np.prod(shape), output_neurons)
         self.dropout = torch.nn.Dropout(p=drop_rate)
 
         self.relu = torch.nn.ReLU()
