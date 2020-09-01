189d097e710a136e10ab2c1675118d94cafdb69b
Mami Hayashida <mhaya2@uky.edu>
2020-09-01 19:30:15 +0000

    Removes output_dir from the constructor; Adds logging.

--- SubvolumeVisualization/inkid_gradcam.py
+++ SubvolumeVisualization/inkid_gradcam.py
@@ -6,6 +6,7 @@ from typing import Union
 import numpy as np
 from pathlib import Path
 from PIL import Image
+import json
 import re
 import os
 import plotly.graph_objects as go
@@ -14,51 +15,61 @@ import cv2
 
 class InkidGradCam:
     '''
-    3D GradCam implementation specifically for Inkid subvolumes.
+    3D GradCam. Current implementation for inkid subvolumes. To make it more
+    universal, a few details will have to be modified.
     
     Attributes
     ----------
-    input_dir(str):  Name of the input directory containing *.tif files
     output_dir(str): Name of the output directory where generated images will be saved.
-    encoder:
-    decoder:
-    saved_model:
-    subvolume (numpy.ndarray): 3D array representing an Inkid subvolume.
-    prediction(int): 0 or 1
-    heatmap(torch tensor): 3D tensor representing "heat" distribution
+    encoder(torch.nn.Module): CNN layers
+    decoder(torch.nn.Module): Fully-connected layers
+    pretrained_model(.pt file): pre-trained model with weights
+    subvolume(torch.Tensor): 5D tensor created when a subvolume is given
+    input_dir(str): Name of the input directory if given
+    __net(torch.nn.Sequential): container for encoder and decoder modules
+    __activations(torch.Tensor): 5D tensor of activations obtained through forward hook
+    __gradients(torch.Tensor): 5D tensor of activations obtained through backward hook
+    prediction(int): 0 (no ink) or 1 (ink)
+    heatmap(torch.Tensor): 3D tensor representing "heat" distribution
+    log(dict): Log for metadata
 
     Methods
     -------
-    TODO:
+    print_encoder():
+    register_hooks(layer_name):
+    print_final_model():
+    push_subvolume_through(input_dir, subvolume):
+    save_images(heatmap, subvolume, superimposed):
+    animate_heatmap(): TODO?
+    save_metadata(): dumps metadata in a json file and saves it in the output_dir
+    load_data():
+    get_prediction():
+    get_gradients():
+    get_activations():
+
     '''
 
-    def __init__(self, output_dir, encoder, decoder, saved_model):
+    def __init__(self, encoder, decoder, pretrained_model):
         '''
         Input:
-            output_dir(str):
             encoder:
             decoder:
-            saved_model(str): path to the .pt file
+            pretrained_model(str): path to the .pt file
             input_dir(str): directory path where input *.tif files are found.
             subvolume(numpy.ndarray): Alternatively, numpy 3D array representing
                                       the subvolume.
         '''
 
-        # Strip the trailing '/' from the output_dir path
-        if output_dir[-1] is '/':
-            output_dir = output_dir[:-1]
-
-        self.output_dir = output_dir
-
 
         # CNN attributes
         self.encoder = encoder
         self.decoder = decoder
-        self.saved_model = saved_model
+        self.pretrained_model = pretrained_model
         
         # Placeholders for subvolume and input
         self.subvolume = None
         self.input_dir = None
+        self.output_dir = None
 
         # Placeholders for the 3DCNN model with hooks
         self.__net = None
@@ -68,6 +79,14 @@ class InkidGradCam:
         # Placeholders for prediction, heatmap
         self.prediction = None
         self.heatmap = None
+
+        # A JSON log will be produced for each output file
+        self.log = {}
+        self.log['encoder'] = str(self.encoder.__class__.__name__)
+        self.log['decoder'] = str(self.decoder.__class__.__name__)
+        self.log['pretrained_model'] = self.pretrained_model
+
+        self.timestamp = datetime.now().strftime("%Y%m%d%H%M%S")
 
 
     def print_encoder(self):
@@ -112,9 +131,11 @@ class InkidGradCam:
 
 
         # Load the given pre-trained model (.pt file) to set the weights.
-        self.__net.load_state_dict(torch.load(self.saved_model, 
+        self.__net.load_state_dict(torch.load(self.pretrained_model, 
                         map_location=torch.device('cpu')), strict=False)
         self.__net.eval()
+
+        self.log['hook_layer'] = layer_name 
 
 
     def print_final_model(self):
@@ -125,12 +146,11 @@ class InkidGradCam:
             print(param_tensor, "\t", self.__net.state_dict()[param_tensor].size())
  
 
+
     def push_subvolume_through(self, input_dir=None, subvolume=None):
         '''
         Pushes the subvolume through the model 
         Inputs:
-        Returns:
-            heatmap
         '''
         # First, process the subvolume data 
         ## Must have either input_dir or subvolume
@@ -144,37 +164,53 @@ class InkidGradCam:
             self.input_dir = input_dir[:-1] if input_dir[-1] is '/' else input_dir
 
             self.subvolume = torch.from_numpy(self.load_data())
+            self.log['input_dir'] = self.input_dir 
 
         else:
             self.input_dir = None
             self.subvolume = torch.from_numpy(subvolume)
+            self.log['input_dir'] = "No input_dir given"
 
         ## add two more axes
         self.subvolume = self.subvolume[np.newaxis, np.newaxis, ...]
+
 
         # Second, push the subvolume through
         self.prediction = self.__net(self.subvolume).argmax(dim=1).item()
 
         self.__net(self.subvolume)[:, self.prediction, :, :].backward()
-        
 
+
+        ## Expression 1 in the paper: Average the channels of the activations
         pooled_gradients = torch.mean(self.__gradients, dim=[0,2,3,4])
-
-        # weight the channels by corresponding gradients
+        
+        
+        ## Espression 2 in the paper: Relu on top of the heatmap 
+        ### weight the channels by corresponding gradients
         for i in range(pooled_gradients.size()[0]): 
             self.__activations[:, i, :, :, :] *= pooled_gradients[i]
 
-        # average the channels of the activations
         heatmap = torch.mean(self.__activations, dim=1).squeeze()
 
-        # relu on top of the heatmap (we are not insterested in negative values)
+        ### (we are not insterested in negative values)
         heatmap = np.maximum(heatmap.detach(), 0)
         
-        # normalize the heatmap
+        ### normalize the heatmap
         self.heatmap = heatmap/torch.max(heatmap)
 
+        return self.heatmap
 
-    def save_images(self, heatmap=True, subvolume=True, superimposed=True):
+
+
+    def save_images(self, output_dir, heatmap=True, subvolume=True, superimposed=True):
+
+        # Strip the trailing '/' from the output_dir path
+        if output_dir[-1] is '/':
+            output_dir = output_dir[:-1]
+
+        self.output_dir = output_dir
+        self.log['output_dir'] = self.output_dir
+
         if not os.path.exists(self.output_dir):
             os.mkdir(self.output_dir)
 
@@ -234,83 +270,22 @@ class InkidGradCam:
             superimposed_img = cv2.addWeighted(gradient_img, 0.35, subvolume_img, 0.65, 0)
             cv2.imwrite(f'{self.output_dir}/superimposed.png', superimposed_img)            
 
-
-    def visualize_heatmap(self):
-        if not os.path.exists(self.output_dir):
-            os.mkdir(self.output_dir)
-
-        # This may be  necessary for orca to work
-        #pio.orca.config.executable = '{path to orca--perhaps inside conda env}'
-        #pio.orca.config.use_xvfb = True
-        #pio.orca.config.save()
-
-        
-        cube_size = self.heatmap.size()[0]
-        X, Y, Z = np.mgrid[0:cube_size:, 0:cube_size, 0:cube_size]
-
-        values = self.heatmap
-        
-        gradient_map = go.Figure(data=go.Volume(
-            x=X.flatten(),
-            y=Y.flatten(),
-            z=Z.flatten(),
-            value=values.flatten(),
-            isomin=0.1,
-            isomax=1.0,
-            opacity=0.2, # needs to be small to see through all surfaces
-            surface_count=8, # needs to be a large number for good volume rendering
-            colorscale = 'rainbow'
-            ))
-        
-        gradient_map.update_layout(showlegend=False)
-        
-        gradient_map.write_image(f"{self.output_dir}/gradient_map.png")
-       
+        self.save_metadata
 
     def animate_heatmap(self):
         # TODO?
         pass
 
 
-    def visualize_subvolume(self):
+    def save_metadata(self):
         '''
-        Generates a Plotly rendition of the input subvolume.
+        Helper function for saving metadata (called within save_images())
         '''
 
-        subvol = torch.squeeze(self.subvolume)
-        cube_size = subvol.size()[0]        
-        X, Y, Z = np.mgrid[0:cube_size, 0:cube_size, 0:cube_size]
-
-
-        subvolume_map = go.Figure(data=go.Volume(
-            x=X.flatten(),
-            y=Y.flatten(),
-            z=Z.flatten(),
-            value=subvol.flatten(),
-            isomin=0.1,
-            isomax=torch.max(subvol).item()*0.9,
-            opacity=0.3, # needs to be small to see through all surfaces
-            surface_count=8, # needs to be a large number for good volume rendering
-            colorscale = 'Greys'
-            ))
-        
-        subvolume_map.update_layout(showlegend=False)
-        subvolume_map.write_image(f"{self.output_dir}/subvolume_map.png")
-
-
-    def visualize_superimposed_image(self):
-        '''
-        Quick and dirty way of superimpsing the two images (gradient and 
-        subvolume maps).
-        '''
-        gradient_img = cv2.imread(f'{self.output_dir}/gradient_map.png')
-        subvolume_img = cv2.imread(f'{self.output_dir}/subvolume_map.png')
-        
-        superimposed_img = cv2.addWeighted(gradient_img, 0.35, subvolume_img, 0.65, 0)
-        cv2.imwrite(f'{self.output_dir}/superimposed.png', superimposed_img)            
-        
-
+        with open(f"{self.output_dir}/{self.timestamp}.json", "w") as outfile: 
+            json.dump(self.log, outfile, indent=2)
     
+
     def load_data(self):
         '''
         A helper function that loads data from an input directory containing 
@@ -345,7 +320,5 @@ class InkidGradCam:
         return self.__activations        
 
 
-    def get_heatmap(self):
-        return self.heatmap
 
 
