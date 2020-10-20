4792974da8bde8b6c7d29a201f5a1aecf5cc0c55
Stephen Parsons <srpa226@uky.edu>
2020-10-20 11:45:43 -0400

    Introduce dependency job that runs summary and upload

--- scripts/singularity/submit.sh
+++ scripts/singularity/submit.sh
@@ -9,7 +9,6 @@
 # Make rclone available on the container and tell system where to look
 #SBATCH --export=SINGULARITY_BIND='/share/singularity/bin',SINGULARITYENV_PREPEND_PATH='/share/singularity/bin'
 
-
 module load ccs/singularity
 
 if [ -z "$SLURM_ARRAY_TASK_ID" ]; then
@@ -17,8 +16,3 @@ if [ -z "$SLURM_ARRAY_TASK_ID" ]; then
 else
     time singularity run --nv --overlay inkid.overlay inkid.sif inkid-train-and-predict "$@" -k $SLURM_ARRAY_TASK_ID
 fi
-
-sbatch --dependency afterany:"$SLURM_JOB_ID" -A col_seales_uksr --mail-type=END --job-name=inkid_summary --output=out/inkid_summary_%A_%a.out \
-  singularity run --overlay inkid.overlay inkid.sif inkid-summary "$2" --all
-
-# TODO rclone
---
+++ scripts/singularity/submit_with_summary.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Run sbatch job as provided by just executing all args
+# (which includes call to submit.sh as the first arg)
+OUTPUT=$("$@")
+# Should print "Submitted batch job <ID>"
+echo $OUTPUT
+# Get the job ID from that string
+JOB_ID=$(echo $OUTPUT | sed 's/[^0-9]*//g')
+
+# Make a new summary job dependent on the first one
+# Shift args until we reach "submit.sh", meaning <infile> <outputDir> follow
+while [ $1 != "submit.sh" ]
+do
+  shift
+done
+# "$3" is now <outputDir> and can be passed to summary script
+OUTPUT2=$(sbatch --dependency afterany:"$JOB_ID" summary.sh "$3")
+# Should print "Submitted batch job <ID>"
+echo $OUTPUT2
