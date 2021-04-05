717e9007a6bdeb3301b14b8539c4da2f4dc44287
Stephen Parsons <srpa226@uky.edu>
2021-04-05 16:46:17 -0400

    Add Cython language directive

--- setup.py
+++ setup.py
@@ -4,8 +4,11 @@ from Cython.Build import cythonize
 import numpy as np
 
 extensions = [
-    Extension('inkid.data.volume', ['inkid/data/volume.pyx'],
-              include_dirs=[np.get_include()]),
+    Extension(
+        'inkid.data.volume',
+        ['inkid/data/volume.pyx'],
+        include_dirs=[np.get_include()]
+    ),
 ]
 
 setup(
