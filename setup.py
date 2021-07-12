04acc6014e8e38d18cd5e7ccd6504f68a73f1707
Jacob Chappell <jacob.chappell@uky.edu>
2021-07-12 10:30:07 -0500

    Use setup.cfg and pyproject.toml over setup.py where possible.

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
-from setuptools import Extension, setup, find_packages
-
+from setuptools import Extension, setup
 from Cython.Build import cythonize
 import numpy as np
 
@@ -9,43 +8,5 @@ extensions = [
 ]
 
 setup(
-    name='inkid',
-    version='0.0.1',
-    description='Identify ink via machine learning.',
-    url='https://code.vis.uky.edu/seales-research/ink-id',
-    author='University of Kentucky',
-    license='GPLv3',
-    packages=find_packages(include=['inkid', 'inkid.*']),
-    install_requires=[
-        'autopep8',
-        'configargparse',
-        'Cython',
-        'dicttoxml',
-        'gitpython',
-        'humanize',
-        'imageio',
-        'jsmin',
-        'mathutils',
-        'matplotlib',
-        'Pillow==8.2.0',  # Temporary due to bug in 8.3.0 https://github.com/pytorch/pytorch/issues/61125
-        'pygifsicle',
-        'pylint',
-        'pywavelets',
-        'scikit-learn',
-        'sphinx',
-        'tensorboard',
-        'torch',
-        'torch-summary',
-        'tqdm',
-        'wand',
-    ],
     ext_modules=cythonize(extensions, annotate=True),
-    entry_points={
-        'console_scripts': [
-            'inkid-train-and-predict = inkid.scripts.train_and_predict:main',
-            'inkid-summary = inkid.scripts.create_summary_images:main',
-            'inkid-rclone-upload = inkid.scripts.rclone_upload:main',
-        ],
-    },
-    zip_safe=False,
 )
