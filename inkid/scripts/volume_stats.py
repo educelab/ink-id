14c25f0e6159617bc351d0b9341a856f54327cbd
Stephen Parsons <srpa226@uky.edu>
2023-01-26 16:17:26 -0500

    More detail in volume stats script. Update deps

--- inkid/scripts/volume_stats.py
+++ inkid/scripts/volume_stats.py
@@ -4,6 +4,7 @@ from pathlib import Path
 import imageio.v3 as iio
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.ndimage import gaussian_filter
 from tqdm import tqdm
 
 
@@ -28,18 +29,37 @@ def main():
     mins = []
     maxs = []
     means = []
+    blurred_mins = []
+    blurred_maxs = []
+    blurred_means = []
 
     for slice_path in tqdm(slice_paths):
-        img = iio.imread(slice_path)
         slice_indices.append(int(slice_path.stem))
+
+        img = iio.imread(slice_path)
         mins.append(np.amin(img))
         maxs.append(np.amax(img))
         means.append(np.mean(img))
 
+        filtered_img = gaussian_filter(img, 3)
+        blurred_mins.append(np.amin(filtered_img))
+        blurred_maxs.append(np.amax(filtered_img))
+        blurred_means.append(np.mean(filtered_img))
+
+    print(f"Global raw min: {np.amin(mins)}")
+    print(f"Global raw max: {np.amax(maxs)}")
+    print(f"Global raw mean: {np.mean(means)}")
+    print(f"Global blurred min: {np.amin(blurred_mins)}")
+    print(f"Global blurred max: {np.amax(blurred_maxs)}")
+    print(f"Global blurred mean: {np.mean(blurred_means)}")
+
     fig, ax = plt.subplots()
-    ax.plot(slice_indices, mins, label="min")
-    ax.plot(slice_indices, maxs, label="max")
-    ax.plot(slice_indices, means, label="mean")
+    ax.plot(slice_indices, mins, label="min", color="blue")
+    ax.plot(slice_indices, maxs, label="max", color="red")
+    ax.plot(slice_indices, means, label="mean", color="green")
+    ax.plot(slice_indices, blurred_mins, label="blurred min", color="lightskyblue")
+    ax.plot(slice_indices, blurred_maxs, label="blurred max", color="lightcoral")
+    ax.plot(slice_indices, blurred_means, label="blurred mean", color="darkseagreen")
 
     ax.legend()
     ax.set_title(f"Stats for volume: {input_slices_dir.stem}")
