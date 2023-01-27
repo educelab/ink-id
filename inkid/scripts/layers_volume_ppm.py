d3ddf2816cc65b96c54266704bdddef35e639c70
Stephen Parsons <srpa226@uky.edu>
2023-01-27 18:29:02 -0500

    layers_volume_ppm script takes in a slice directory instead of single stacked tif

--- inkid/scripts/layers_volume_ppm.py
+++ inkid/scripts/layers_volume_ppm.py
@@ -1,6 +1,6 @@
 import argparse
+from pathlib import Path
 
-import imageio.v3 as iio
 from tqdm import tqdm
 
 from inkid.data import PPM
@@ -8,18 +8,19 @@ from inkid.data import PPM
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--input-tif-stack", required=True)
+    parser.add_argument("--input-tif-directory", required=True)
     parser.add_argument("--input-ppm", required=True)
     parser.add_argument("--output-ppm", required=True)
     args = parser.parse_args()
 
-    tif_stack = iio.imread(args.input_tif_stack)
+    input_tif_dir = Path(args.input_tif_directory)
+    num_slices = len(list(input_tif_dir.glob("*.tif")))
 
     print("Reading input PPM...")
     ppm = PPM.from_path(args.input_ppm)
 
     print("Modifying PPM...")
-    z = (tif_stack.shape[0] - 1) / 2.0
+    z = (num_slices - 1) / 2.0
     for y in tqdm(range(ppm.height)):
         for x in range(ppm.width):
             ppm.data[y, x, :] = [x, y, z, 0.0, 0.0, 1.0]
