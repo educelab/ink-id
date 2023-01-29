846e960520281a58688ae480bbf890db343883a4
Stephen Parsons <srpa226@uky.edu>
2023-01-29 16:35:58 -0500

    Add 1/99 percentile calculations to hdf min/max script

--- inkid/scripts/sample_hdf_min_max.py
+++ inkid/scripts/sample_hdf_min_max.py
@@ -22,6 +22,8 @@ def main():
         global_raw_max = -np.inf
         global_blurred_min = np.inf
         global_blurred_max = -np.inf
+        global_percentile_min = np.inf
+        global_percentile_max = -np.inf
 
         for i in range(slices):
             z = random.randint(0, slices - 1)
@@ -32,6 +34,8 @@ def main():
             raw_max = np.amax(img)
             blurred_min = np.amin(blurred_img)
             blurred_max = np.amax(blurred_img)
+            percentile_min = np.percentile(img, 1)
+            percentile_max = np.percentile(img, 99)
 
             update = False
             if raw_min < global_raw_min:
@@ -46,6 +50,12 @@ def main():
             if blurred_max > global_blurred_max:
                 global_blurred_max = blurred_max
                 update = True
+            if percentile_min < global_percentile_min:
+                global_percentile_min = percentile_min
+                update = True
+            if percentile_max > global_percentile_max:
+                global_percentile_max = percentile_max
+                update = True
 
             if update:
                 print(f"Slices processed: {i}", flush=True)
@@ -53,11 +63,15 @@ def main():
                 print(f"Raw max: {global_raw_max}", flush=True)
                 print(f"Blurred min: {global_blurred_min}", flush=True)
                 print(f"Blurred max: {global_blurred_max}", flush=True)
+                print(f"Percentile min: {global_percentile_min}", flush=True)
+                print(f"Percentile max: {global_percentile_max}", flush=True)
 
         print(f"Raw min: {global_raw_min}")
         print(f"Raw max: {global_raw_max}")
         print(f"Blurred min: {global_blurred_min}")
         print(f"Blurred max: {global_blurred_max}")
+        print(f"Percentile min: {global_percentile_min}")
+        print(f"Percentile max: {global_percentile_max}")
 
 
 if __name__ == "__main__":
